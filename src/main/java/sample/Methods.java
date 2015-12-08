6cfdd6119cacd3e621aee89f275ef4cae4c1c54b
Jebin Jose <jebin_2004@hotmail.com>
2015-12-08 21:52:14 +0000

    Methods

---
+++ src/main/java/sample/Methods.java
@@ -0,0 +1,17 @@
+package sample;
+//import java.util.Scanner;
+
+public class Methods {
+	public static void helloWorld(String name){
+//		System.out.println("Name ;");
+//		Scanner sc = new Scanner(System.in);
+//		String name = sc.nextLine();
+//		sc.close();
+		System.out.println("Hello " + name);
+	}
+	
+	public static void main(String[] args) {
+		Methods.helloWorld("David");
+	}	
+	
+}
