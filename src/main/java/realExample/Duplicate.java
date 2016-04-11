8109fb757f48f923daeaae181d93a19535638d6b
Jebin Jose <jebin_2004@hotmail.com>
2016-04-11 21:16:06 +0100

    New code added

---
+++ src/main/java/realExample/Duplicate.java
@@ -0,0 +1,18 @@
+package realExample;
+
+import java.util.Arrays;
+
+public class Duplicate {
+	public static void main(String args[]) {
+
+		int[] input = { 1, 5, 23, 5, 23, 2, 1, 6, 12, 3, 1, 8, 12, 3, 23 };
+
+		Arrays.sort(input);
+
+		for (int i = 1; i < input.length; i++) {
+			if (input[i] == input[i - 1]) {
+				System.out.println("Duplicate" + input[i]);
+			}
+		}
+	}
+}
